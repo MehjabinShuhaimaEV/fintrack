ab62f048f63d175491379e04fb060cd5f1f14ed2
Mehjabin Shuhaima EV <158308765+MehjabinShuhaimaEV@users.noreply.github.com>
2026-03-07 11:04:30 +0530

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'fintrack',
+        'password' => 'Fintrack@2024',
+        'database' => 'fintrack',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
